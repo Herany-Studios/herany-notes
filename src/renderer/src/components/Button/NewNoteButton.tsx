2a46905af0a2736cdd1753ca305093505230e0f9
DanPeregrineDev <madeira.daf@gmail.com>
2024-06-17 02:20:04 +0100

    Added button functionality

--- src/renderer/src/components/Button/NewNoteButton.tsx
+++ src/renderer/src/components/Button/NewNoteButton.tsx
@@ -1,9 +1,17 @@
 import { ActionButton, ActionButtonProps } from '@/components'
+import { createEmptyNoteAtom } from '@renderer/store'
+import { useSetAtom } from 'jotai'
 import { GrAdd } from 'react-icons/gr'
 
 export const NewNoteButton = ({ ...props }: ActionButtonProps) => {
+  const createEmptyNote = useSetAtom(createEmptyNoteAtom)
+
+  const handleCreation = () => {
+    createEmptyNote()
+  }
+
   return (
-    <ActionButton {...props}>
+    <ActionButton onClick={handleCreation} {...props}>
       <GrAdd className="w-4 h-4 text-zinc-300" />
     </ActionButton>
   )
