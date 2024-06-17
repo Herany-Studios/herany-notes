881ac4e2979c4e60a8edc4ef3526877658dd4735
DanPeregrineDev <madeira.daf@gmail.com>
2024-06-17 03:19:50 +0100

    File Read System

--- src/main/index.ts
+++ src/main/index.ts
@@ -1,4 +1,6 @@
+import { getNotes, readNote } from '@/lib'
 import { electronApp, is, optimizer } from '@electron-toolkit/utils'
+import { GetNotes, ReadNote } from '@shared/types'
 import { BrowserWindow, app, ipcMain, shell } from 'electron'
 import { join } from 'path'
 import icon from '../../resources/icon.png?asset'
@@ -60,6 +62,9 @@ app.whenReady().then(() => {
 
   // IPC test
   ipcMain.on('ping', () => console.log('pong'))
+
+  ipcMain.handle('getNotes', (_, ...args: Parameters<GetNotes>) => getNotes(...args))
+  ipcMain.handle('readNote', (_, ...args: Parameters<ReadNote>) => readNote(...args))
 
   createWindow()
 
--- src/preload/index.d.ts
+++ src/preload/index.d.ts
@@ -1,8 +1,11 @@
 import { ElectronAPI } from '@electron-toolkit/preload'
+import { GetNotes, ReadNote } from '@shared/types'
 
 declare global {
   interface Window {
     electron: ElectronAPI
+    getNotes: GetNotes
+    readNote: ReadNote
     api: unknown
   }
 }
