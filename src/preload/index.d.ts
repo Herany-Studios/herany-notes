eddb68255a6bdd4644487a9e58d345efa6720d69
DanPeregrineDev <madeira.daf@gmail.com>
2024-06-17 11:06:50 +0100

    Autosaving

--- src/preload/index.d.ts
+++ src/preload/index.d.ts
@@ -1,11 +1,12 @@
 import { ElectronAPI } from '@electron-toolkit/preload'
-import { GetNotes, ReadNote } from '@shared/types'
+import { GetNotes, ReadNote, WriteNote } from '@shared/types'
 
 declare global {
   interface Window {
     electron: ElectronAPI
     getNotes: GetNotes
     readNote: ReadNote
+    writeNote: WriteNote
     api: unknown
   }
 }
--- src/shared/constants.ts
+++ src/shared/constants.ts
@@ -1,1 +1,2 @@
 export const fileEncoding = 'utf8'
+export const autoSavingTime = 3000
